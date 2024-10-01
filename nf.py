6d471342baafaced467b5cdd4d36cf6a341ff2c1
Mateus Santos <115822261+MateussSantos1@users.noreply.github.com>
2024-09-30 21:07:44 -0300

    finalizado?

--- nf.py
+++ nf.py
@@ -3,11 +3,16 @@ import pandas as pd
 import unicodedata
 from PyPDF2 import PdfWriter, PdfReader
 
+
+# ANTES DE TUDO, JOGAR O SALSICHAO DOS PDF´S NO SITE OCR PDF24 E FORÇAR OCR NA LINGUA PORTUGUESA
+
+
+
 # Caminho para o PDF e a planilha
-arq_pdf = r"C:\Users\mateus.santos.AD\Pictures\NOTA FISCAL AUTOM\nfexemp.pdf"
+arq_pdf = r"E:\teste_tesseract_gif\nfjun.pdf"
 
 
-planilha = r"C:\Users\mateus.santos.AD\Pictures\NOTA FISCAL AUTOM\Sodexo.xlsx"
+planilha = r"E:\teste_tesseract_gif\sla.xlsx"
 
 # Carregando a planilha
 planilha_df = pd.read_excel(planilha)
@@ -23,24 +28,29 @@ for codigo in codigos_doc:
 
 
     # Selecionando a linha correspondente ao código
-    print(f"Código é{codigo}")
+
     linha_selecionada = planilha_df.loc[planilha_df['Número do Documento'] == codigo]
-    print(f"linha selecionada é {linha_selecionada}")
 
-    codigo = codigo[3:]  # Atualizado para remover antes de procurar no PDF
-    codigo_com_nf = f"nf={codigo}"
 
+    # Convertendo o código para string antes de remover os 3 primeiros dígitos
+    codigo = str(codigo)[3:]    
+    
+    # Atualizado para remover antes de procurar no PDF
 
     
     if not linha_selecionada.empty:
 
-        print('olá')
+
         # Obtendo o índice da primeira linha encontrada
         indice = linha_selecionada.index[0]
+
         
         referencia = unicodedata.normalize('NFKD', linha_selecionada['Histórico'].values[0]).encode('ASCII', 'ignore').decode('ASCII')
+        
         obra = unicodedata.normalize('NFKD', linha_selecionada['Centro de Custo'].values[0]).encode('ASCII', 'ignore').decode('ASCII')
+        
         valor = linha_selecionada['Valor líquido'].values[0]
+        
 
         paginas = PdfReader(arq_pdf).pages
         encontrei_codigo = False
@@ -48,7 +58,7 @@ for codigo in codigos_doc:
         for pagina in paginas:
             texto_pagina = pagina.extract_text()
 
-            if codigo_com_nf in texto_pagina:
+            if codigo in texto_pagina:
                 conteudo_pdf_novo = PdfWriter()
                 conteudo_pdf_novo.add_page(pagina)
                 nome_novo_pdf = f"{referencia} -- {obra} -- {valor}.pdf"
@@ -56,14 +66,15 @@ for codigo in codigos_doc:
                 with open(nome_novo_pdf, "wb") as novo_pdf:
                     conteudo_pdf_novo.write(novo_pdf)
 
-                print(f"Arquivo {codigo} gerado com sucesso!")
+                print(f"Arquivo {codigos_doc} gerado com sucesso!")
                 encontrei_codigo = True
                 break
 
         if not encontrei_codigo:
             codigos_nao_encontrados.append(codigo)
             cont_nao_encontrados += 1
-            print(f"{codigo} não foi encontrado no PDF!!!")
+            print(f"{codigos_doc} não foi encontrado no PDF!!!")
+            break
 
 
 
@@ -75,8 +86,7 @@ print(f"No total, não foram encontrados os comprovantes de {cont_nao_encontrado
 # Criação de DataFrame com os códigos não encontrados
 df_codigos_nao_encontrados = pd.DataFrame({'CODIGOS NÃO ENCONTRADOS': codigos_nao_encontrados})
 
-diretorio = r'C:\Users\mateus.santos.AD\Pictures\NOTA FISCAL AUTOM'
+diretorio = r'E:\teste_tesseract_gif'
 arquivo_saida = os.path.join(diretorio, 'Codigos_nao_encontrados.xlsx')
 df_codigos_nao_encontrados.to_excel(arquivo_saida, index=False)
 print(f'Foi gerado um arquivo Excel com os códigos não encontrados salvo em: {arquivo_saida}')
-
